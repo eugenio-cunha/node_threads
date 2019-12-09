1eeafda9d30204a1a77ccf613d335bea360cff26
eugenio.cunha <eugenio.cunha@ymail.com>
2019-12-09 16:08:59 -0300

    codigo refatorado

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const request = require('request')
 
 function startWorker(path, cb) {
   const worker = new Worker(path, { workerData: null })
-  worker.on('message', (msg) => {
-    cb(null, msg)
-  })
+  worker.on('message', msg => cb(null, msg))
+  
   worker.on('error', cb)
-  worker.on('exit', (code) => {
-    if (code != 0)
-      console.error(new Error(`Worker finalizado com exit code = ${code}`))
+  
+  worker.on('exit', code => {
+    if (code != 0) console.error(new Error(`Worker finalizado com exit code = ${code}`))
   })
   return worker
 }
 
-console.log("Thread principal")
-
 // Inicia o worker em outra thread
-startWorker(__dirname + '/worker-code.js', (err, result) => {
+startWorker(`${__dirname}/worker-code.js`, (err, result) => {
   if (err) return console.error(err)
-  console.log("** COMPUTAÇÃO PESADA FINALIZADA **")
+
   console.log(`Duração = ${(result.end - result.start) / 1000} segundos`)
+  console.log('** THREAD COMPUTAÇÃO PESADA FINALIZADA **')
 })
 
+console.log('Thread principal')
+
 // Continua com o a execução na thread principal
-request.get('https://www.google.com', (err, resp) => {
+request.get('http://www.eugenio.xyz', (err, resp) => {
   if (err) return console.error(err)
   console.log(`Total bytes recebidos = ${resp.body.length}`)
 })
--- worker-code.js
+++ worker-code.js
@@ -1,7 +1,10 @@
+'use strict';
+
 const { parentPort } = require('worker_threads')
 
 const start = Date.now()
 
+console.log('** THREAD COMPUTAÇÃO PESADA INICIADA **')
 // Simulando computação "pesada"
 for (let i = 0; i < 1000000; i++) {
   for (let j = 0; j < 10000; j++) {
